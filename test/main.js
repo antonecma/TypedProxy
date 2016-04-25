f8ca29d12a0d3475d1b3b98dd783cc914454469c
Anton L <anton.ecma@gmail.com>
2016-04-25 15:59:18 +0400

    typedProxy start

--- test/main.js
+++ test/main.js
@@ -1,12 +1,12 @@
 'use strcit';
 
 const should = require('should');
-const typed = require('../typed.js');
+const Typed = require('../typed.js');
 
 describe('Typed', () => {
     describe('proxy', () => {
         it('should return new instance of TypedObject', () => {
-            (new typed()).should.be.eql({rules : {}});
+            (new Typed()).should.be.eql({name : 'Test Class'});
         });
     });
 });
--- typed.js
+++ typed.js
@@ -1,2 +1,20 @@
 'use strict';
 
+class TestClass {
+    constructor(){
+        this.name = 'Test Class';
+    }
+}
+class TypedProxy {
+    constructor() {
+        const handler = {
+            construct : (target, argList) => {
+                return new target(...argList);
+            }
+        };
+        const proxy = new Proxy(TestClass, handler);
+        return new proxy();
+    }
+}
+module.exports =  TypedProxy;
+
