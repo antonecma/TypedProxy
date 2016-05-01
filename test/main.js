e55e0b54960a869500127c9643ab86eee880ff8e
Anton L <anton.ecma@gmail.com>
2016-05-02 00:19:54 +0400

    start working with Proxy.

--- test/main.js
+++ test/main.js
@@ -1,12 +1,51 @@
 'use strcit';
 
 const should = require('should');
+const assert = require('assert');
 const Typed = require('../typed.js');
 
+class TestClass {
+    constructor(strName){
+        this.soname = strName;
+    }
+    saySomething(strSomeString){
+        return strSomeString;
+    }
+    static staticMethod(strSomeStaticString) {
+        return strSomeStaticString;
+    }
+}
+
 describe('Typed', () => {
-    describe('proxy', () => {
-        it('should return new instance of TypedObject', () => {
-            (new Typed()).should.be.eql({name : 'Test Class'});
+    describe('constructor', () => {
+        it('should throw type error when invoke without typedClass as first param', ()=>{
+            (() => { new Typed(); }).should.throw();
+        });
+        it('should throw type error when invoke when first param is not Function instance', ()=>{
+            (() => { new Typed(1); }).should.throw();
+        });
+    });
+    describe('new()', () => {
+        it('should invoke through proxy', () => {
+            const constructorParam = 'Anton';
+            const TypedTestClass = new Typed(TestClass);
+            (new TypedTestClass(constructorParam)).should.be.eql(new TestClass(constructorParam));
+        });
+        it('should throw error with bad construct param', () => {
+            const TypedTestClass = new Typed(TestClass);
+            (() => { new TypedTestClass(1); }).should.throw(TypeError);
+        });
+        it('should throw error with bad length of  construct param', () => {
+            const TypedTestClass = new Typed(TestClass);
+            const constructorParam = 'Anton';
+            (() => { new TypedTestClass(constructorParam, constructorParam); }).should.throw(RangeError);
+        });
+    });
+    describe('static methods', () => {
+        it('should invoke static method', () => {
+            const TypedTestClass = new Typed(TestClass);
+            const methodParam = 'something';
+            (TypedTestClass.staticMethod(methodParam)).should.be.equal(methodParam);
         });
     });
 });
--- typed.js
+++ typed.js
@@ -1,19 +1,108 @@
 'use strict';
 
-class TestClass {
-    constructor(){
-        this.name = 'Test Class';
+function getFunctionArgumentsDescribe(func) {
+    return (func+'').replace(/\s+/g,'')
+        .replace(/[/][*][^/*]*[*][/]/g,'') // strip simple comments
+        .split('){',1)[0].replace(/^[^(]*[(]/,'') // extract the parameters
+        .replace(/{+/g,'') // strip any ES6 defaults
+        .replace(/}+/g,'') // strip any ES6 defaults
+        .replace(/=[^,]+/g,'') // strip any ES6 defaults
+        .split(',').filter(Boolean); // split & filter [""]
+};
+
+const types = {
+    'str' : (value) => {
+
+        let typeOfValue = {}.toString.call(value).slice(8, -1);
+
+        if(typeOfValue !== 'String'){
+            throw new TypeError(`str parameter must be String instance, not ${typeOfValue}`);
+        }
     }
-}
-class TypedProxy {
-    constructor() {
-        const handler = {
-            construct : (target, argList) => {
-                return new target(...argList);
+};
+
+const typeTester = (functionName, ...passedArgs) => {
+    //compare length of function passed args and length of expected args
+    const argsLength = passedArgs.length;
+    const typesOfParams = getFunctionArgumentsDescribe(functionName);
+
+    if(argsLength !== typesOfParams.length) {
+        throw new RangeError('amount of parameters is invalid');
+    } else {
+
+        const allTypes = Object.keys(types);
+        const allTypesLength = allTypes.length;
+
+        for(let i = 0; i < argsLength; i++){
+            let typeDefinedInTypesObject = false;
+            for(let j = 0; j < allTypesLength; j++){
+                if(typesOfParams[i].startsWith(allTypes[j])){
+                    typeDefinedInTypesObject = true;
+                    types[allTypes[j]](passedArgs[i]);
+                    break;
+                }
+            }
+            if(!typeDefinedInTypesObject) {
+                throw new RangeError(`can not find type of ${typesOfParams[i]} value in [${allTypes}]`);
             }
         };
-        const proxy = new Proxy(TestClass, handler);
-        return new proxy();
+
+        return true;
+    }
+};
+
+class TypedProxy {
+    constructor(typedClass) {
+        /*
+        Work with Class only
+         */
+        if(! (typedClass instanceof Function)){
+            throw new TypeError('typedClass must be a Function instance');
+        }
+        /*
+        Determinate static methods
+         */
+        const staticMethodOfTypedClass = Object.getOwnPropertyNames(typedClass).filter((property) => {
+            const descOfProp = Object.getOwnPropertyDescriptor(typedClass, property);
+            return (descOfProp.value instanceof Function) && descOfProp.writable && descOfProp.configurable;
+        });
+        staticMethodOfTypedClass.forEach((methodName) => {
+            const handler = {
+                apply(target, thisArgument, argList){
+
+                }
+            };
+            const proxy = new Proxy();
+        });
+        const handler = {
+            construct(target, argList) {
+                /*
+                    Interception for 'new' statement
+                    Here we checks argList with typeTester()
+                  */
+                typeTester(typedClass, ...argList);
+                const instance = new target(...argList);
+                const instanceProxyHandler = {
+                    get(target, property) {
+                        return target[property];
+                    }
+                };
+                const proxyInstance = new Proxy(instance, instanceProxyHandler);
+                return proxyInstance;
+
+            },
+            get(target, prop){
+                /*
+                static methods
+                 */
+                if(staticMethodOfTypedClass.includes(prop)){
+                    typeTester(target[prop])
+                }
+                return target[prop];
+            }
+        };
+        const proxy = new Proxy(typedClass, handler);
+        return proxy;
     }
 }
 module.exports =  TypedProxy;
