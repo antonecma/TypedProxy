6f9cffd13d83321f2de0e50fee3ff1fa5dc147b7
Anton L <anton.ecma@gmail.com>
2016-05-10 00:47:23 +0400

    license added

--- typed.js
+++ typed.js
@@ -1,3 +1,26 @@
+/*
+ MIT License
+
+ Copyright (c) [2016] [Anton L.]
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE
+ */
 'use strict';
 
 const getFunctionArgumentsDescribe = (func) => {
@@ -99,6 +122,7 @@ const arrOfAllOwnWritableAndConfigurableMethods = (obj) => {
         return (descOfProp.value instanceof Function) && descOfProp.writable && descOfProp.configurable;
     });
 };
+
 class TypedProxy {
     constructor(typedClass, types = defaultTypes, typeTester = typeTesterDefault) {
         /*
@@ -149,7 +173,6 @@ class TypedProxy {
                 const methodsOfTypeInstance = arrOfAllOwnWritableAndConfigurableMethods(Object.getPrototypeOf(instance));
                 const mapSettersOfInstance = mapOfAllOwnSetters(instance);
                 const excludeMethods = ['constructor'];
-                //TODO [Symbol.someSymbol] does not have place in map below
                 const mapOfMethodsAndThemProxies = mapApplyHandler(instance, methodsOfTypeInstance, ...excludeMethods);
                 const instanceHandler = {
                     get(target, prop){
